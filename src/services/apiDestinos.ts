9ee285d3e203a03819b1e6e51c1132d856839e24
ezeAst <166661587+ezeAst@users.noreply.github.com>
2025-11-26 18:11:18 -0500

    asd

--- src/services/apiDestinos.ts
+++ src/services/apiDestinos.ts
@@ -1,4 +1,5 @@
-const API_BASE = import.meta.env.VITE_API_BASE;
+//const API_BASE = import.meta.env.VITE_API_URL || '/api';
+const API_BASE = "http://localhost:8080/api"
 
 
 export async function getDestinos(): Promise<string[]> {
